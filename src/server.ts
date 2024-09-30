299fdd43b8a63bb57bd0778e26bf5cbfd1102116
Samin Israr Ravi <saminisrar1@gmail.com>
2024-09-30 14:57:57 +0600

    winston

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { Server } from "http";
 import app from "./app";
+import { errorlogger, logger } from "./app/src/shared/logger";
 
 let server: Server;
 
@@ -7,9 +8,11 @@ async function main() {
   try {
     server = app.listen(5000, () => {
       console.log(`app is listening on port 5000`);
+      logger.info(`app is listening on port 5000`);
     });
   } catch (err) {
     console.log(err);
+    errorlogger.error(err);
   }
 }
 
@@ -17,6 +20,7 @@ main();
 
 process.on("unhandledRejection", (err) => {
   console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+  errorlogger.error(err);
   if (server) {
     server.close(() => {
       process.exit(1);
@@ -27,5 +31,6 @@ process.on("unhandledRejection", (err) => {
 
 process.on("uncaughtException", () => {
   console.log(`😈 uncaughtException is detected , shutting down ...`);
+  errorlogger.error("uncaughtException is detected");
   process.exit(1);
 });
